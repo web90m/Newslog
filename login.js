2a46e6014485e299c5ac71318997df7ca4caa2d1
apps <iyasu4313@gmail.com>
2026-02-04 09:22:23 +0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,14 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+window.login = function(){
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      location.href = "dashboard.html";
+    })
+    .catch(err => alert(err.message));
+}
